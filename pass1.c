86d0d62055db64a76ed5a01cc1a925234f7944a2
NIKHIL KUMAR <nikhilkumar_15508@aitpune.edu.in>
2018-02-22 01:38:34 +0530

    Add files via upload

--- pass1.c
+++ pass1.c
@@ -1,382 +1,365 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
-#include<ctype.h>
+#include<string.h>
+FILE *fp,*ip,*tp,*tp1,*tp2;
+char MOT[][11]={"STOP","ADD","SUB","MULT","MOVER","MOVEM","COMP","BC","DIV","READ","PRINT"};
+char POT[][7]={"START","END","LTORG","EQU","ORIGIN"};
+char reg[][5]={"AREG","BREG","CREG","DREG"};
+char decl[][3]={"DC","DS"};
 
-
-//to store all words in a line
-char label[4][10];
-int location_counter =0;
-int literal_no1=1;
-int literal_no2=1;
-
-int L=0; // keeping track of the literals
-int C=0; // keeping track of the constants
-int cnt=0;
-
-int s=0;	// for symbols
-
-
-FILE  *fp2,*fp3;
-
-
-struct mnemonic_tab
+int POOLTAB[10];
+int countl=0,counts=0,countp=0;
+typedef struct symbol
 {
-	int mno;
-	char mcode[10];
-	int ocode; 	// instruction opcode
-	int length; // by default it is 1
-	char type[5]; // TYPE OF THE STATEMENT;
-};
-
-struct mnemonic_tab mtable[20];
-
-struct sym_tab
+	int s_no;
+	char s_name[200];
+	int s_add;
+}symbol;
+typedef struct litral
 {
-	int sno;  // Symbol no
-	char sname[10]; //symbol name
-	int saddr;  // symbol Address
-	int slength;
-};
+	int l_no;
+	char l_name[100];
+	int l_add;
+}litral;
+	symbol s[10];
+	litral l[10];
 
-struct sym_tab symbol_tab[10];
-
-
-void initialize_mtable()
+int search_POT(char tok1[10])
 {
-	int i;
-	int j=1;
-// listing all the assembler directives
-	strcpy(mtable[0].mcode, "START");
-	strcpy(mtable[1].mcode, "END");
-	strcpy(mtable[2].mcode,"ORIGIN");
-	strcpy(mtable[3].mcode,"LTORG");
-	strcpy(mtable[4].mcode,"EQU");
-
-
-	for(i = 0; i<=4;i++)
+	int i,e;
+	for(i=0;i<5;i++)
 	{
-		mtable[i].mno=i;
-		mtable[i].ocode=i+1;
-		mtable[i].length=0;
-		strcpy(mtable[i].type,"AD");
+		e=strcmp(tok1,POT[i]);
+		if(e==0)
+			return (i+1);
 	}
-
-// NOW FOR DECLARATIVE STATMENTS
-
-	strcpy(mtable[5].mcode,"DC");
-	strcpy(mtable[6].mcode,"DS");
-
-
-	while(i<=6)
-	{
-		mtable[i].mno=i+1;
-		mtable[i].ocode=i+1;
-		strcpy(mtable[i].type,"DL");
-		i++;
-	}
-
-// now imerative statemnts
-	strcpy(mtable[7].mcode, "MOVER");
-	strcpy(mtable[8].mcode, "MOVEM");
-	strcpy(mtable[9].mcode, "ADD");
-	strcpy(mtable[10].mcode,"MUL");
-	strcpy(mtable[11].mcode,"SUB");
-	strcpy(mtable[12].mcode, "DIV");
-	strcpy(mtable[13].mcode,"BC");
-	strcpy(mtable[14].mcode, "COMP ");
-
-	while(i<=14)
-	{
-		mtable[i].mno=i;
-		mtable[i].ocode=j; //jkfjdd  .ocode=j;
-		mtable[i].length=1;
-		strcpy(mtable[i].type, "IS");
-		i++;
-		j++;
-	}
-
-}
-
-int find(char check[])
-{
-	int i;
-
-	for(i=0;i<15;i++)
-	{
-		if( strcmp (check,mtable[i].mcode) == 0 )
-			return i;
-	}
-
-	if(strcmp(check, "AREG")==0)
-		return 51;
-	if(strcmp(check, "BREG")==0)
-		return 52;
-	if(strcmp(check,"CREG")==0)
-		return 53;
-	if(strcmp(check,"DREG")==0)
-		return 54;
-
 	return -1;
-
-}
-
-void label_initialize()
+}	
+int search_MOT(char tok1[10])
 {
-	int i; 
+	int i,e;
+	for(i=0;i<11;i++)
+	{
+		e=strcmp(tok1,MOT[i]);
+		if(e==0)
+			return (i+1);
+	}
+	return -1;
+}
+int search_REG(char tok1[10])
+{
+	int i,e;
 	for(i=0;i<4;i++)
 	{
-		strcpy(label[i]," ");
+		e=strcmp(tok1,reg[i]);
+		if(e==0)
+			return (i+1);
 	}
-
+	return -1;
+}
+int search_dl(char tok1[10])
+{
+	int i,e;
+	for(i=0;i<2;i++)
+	{
+		e=strcmp(tok1,decl[i]);
+		if(e==0)
+			return (i+1);
+	}
+	return -1;
+}
+int search_st(char tok3[10],int counts)
+{
+	int i,e;
+	for(i=0;i<=counts;i++)
+	{
+		e=strcmp(tok3,s[i].s_name);
+		if(e==0)
+			return i;
+	}
+	return (-1);
+	
 }
 
-void labels()
-{
+
+
+int pass1()
+{	
+	int i,lc=0,ch,m,m1,m2,m3,litral=0,ltorg=0,sadd,a=0,m4,m5;
+	char buffer[80],tok1[10],tok2[10],tok3[10],tok4[10];
 	
-	FILE *fp1,*fp4;
-	char temp[30],tmp[10];
-	int i=0,j=0;
-	int flag=0;
-	initialize_mtable();
 	
-	fp1=fopen("input2.txt","r");
-	fp2=fopen("intermediate_code.txt","a+");
-	fp3=fopen("literal.txt","a+");
- 
-	while( !feof(fp1) )
-			 //while(2)
+	while(fgets(buffer,80,fp))
 	{
-		i=0;
-		cnt=0;
-		L=0;
-		C=0;
-		fgets(temp,30,fp1);
-		label_initialize();
-		
-		while(temp[i]!='\n') //while(1)
+		ch=sscanf(buffer,"%s%s%s%s",tok1,tok2,tok3,tok4);
+		switch(ch)
 		{
-			j=0;
-			while(isalnum(temp[i]))
-			{
-				flag=1;
-				tmp[j]=temp[i];
-			
-				if(isdigit(tmp[0]) && temp[i-1]=='\'') //For literals
-						L=1;
-				else if(isdigit(tmp[0]) && temp[i-1]==' ') //For Constants
-						C=1;
-		
-				i++;
-				j++;
-			}
-			if(flag==1)
-			{
-				flag=0;
-				tmp[j]='\0';
-				strcpy(label[cnt],tmp);
-				cnt++;
-				j=0;
-			}
-
-			if(temp[i]=='\n')
-				break;
-
-			i++;
-			}//end of while(1)
-			
-			pass1();
-
-		}//end of while(2)
-
-	fclose(fp1);
-	fclose(fp2);
-	fclose(fp3);
-
-	fp4=fopen("symbol.txt","w");
-
-	fputs("No\tSymbol\t\tlength\t\tAddress\n\n",fp4);
-
-	for(i=0;i<s;i++)
-	{
-		fprintf(fp4,"%d\t",symbol_tab[i].sno);
-		fprintf(fp4,"%s\t\t",symbol_tab[i].sname);
-		fprintf(fp4,"%d\t\t",symbol_tab[i].slength);
-		fprintf(fp4,"%d\n\n",symbol_tab[i].saddr);
-	}
-	
-	fclose(fp4);
-}
-
-void pass1()
-{
-	int check,count=0,value=0,length=1,flag=0,i=0;
-	char c;
-
-	while(count<cnt)
-	{
-		check=find(label[count]);
-		if(isdigit(label[count][0])) //To convert a string with digits
-			value=atoi(label[count]);//into integer value
-
-		if(check==-1 && isalpha(label[count][0]) ) //for symbols
-		{
-			for(i=0;i<s;i++)
-			{
-				if(strcmp(label[count],symbol_tab[i].sname)==0)
+			case 1:
+				//ltorg & END
+				m=search_POT(tok1);
+				POOLTAB[countp]=l[a].l_no;
+				a=a+litral;
+				countp++;
+				litral=0;
+				if(m==2)//end
 				{
-					symbol_tab[i].saddr=location_counter;
-				
-					flag=1;
-					break;
+					for(i=POOLTAB[countp-1];i<=countl;i++)
+					{
+						l[i-1].l_add=lc;
+						fprintf(ip,"%d (AD,0%d) (L,%d)\n",lc,m,i);
+						lc++;
+					}
+					
 				}
-			}
-			if(flag==0)
-			{	
-				symbol_tab[s].sno=s+1;
-				strcpy(symbol_tab[s].sname,label[count]);
-				symbol_tab[s].saddr=location_counter;
-				symbol_tab[s].slength=length;
 				
-				s++;
-			}
-
-			if(count!=0)
-			{					//To print symbols
-				c='S';
-				fprintf(fp2,"\t%c\t",c);
-				if(flag==0)
-					fprintf(fp2,"%d",s);	
-				else
-					fprintf(fp2,"%d",symbol_tab[i].sno);
-			}
-		}
-		else if(check==0) //for Start(AD)
-		{
-			fprintf(fp2,"\t");
-			fprintf(fp2,"%s\t",mtable[check].type);
-			fprintf(fp2,"%d",mtable[check].ocode);
-			count++;
-	
-			value=atoi(label[count]);
- 
-			location_counter=value;
-			c='C';
-			fprintf(fp2,"\t\t%c\t",c);
-			fprintf(fp2,"%d",value);
-			location_counter--;
-		}
-		else if(check>=1 && check<=4)     //for Assembler Directives
-		{
-			if(check==4)//for equ AD
-			{
-				count++;
-				for(i=0;i<s;i++)
+				else //ltorg
 				{
-					if(strcmp(label[count],symbol_tab[i].sname)==0)
-							break;
+					if(ltorg==0)
+					{
+						for(i=0;i<countl;i++)
+						{
+							l[i].l_add=lc;
+							
+							fprintf(ip,"%d (AD,0%d) (L,%d)\n",lc,m,i+1);
+							lc++;
+							
+						}
+					}
+					else
+					{
+						for(i=POOLTAB[countp-1];i<=countl;i++)
+						{
+							l[i-1].l_add=lc;
+							fprintf(ip,"%d (AD,0%d) (L,%d)\n",lc,m,i);
+							lc++;
+						}
+					}
+					ltorg++;
+				
 				}
-			symbol_tab[s-1].saddr=symbol_tab[i].saddr;
-			}
-			location_counter--;
+				
+				
 			break;
-		}
-		else if(check>=7 && check<=14)//for Imperative Statements
-		{
-			fprintf(fp2,"%d",location_counter); //location counter
-		
-			fprintf(fp2,"\t");
-			fprintf(fp2,"%s\t",mtable[check].type); //IS
-			fprintf(fp2,"%d",mtable[check].ocode); //Opcode
-		}
-		else if(check==5 && check==6)//for Declarative Statements
-		{
-			fprintf(fp2,"%d",location_counter); //location counter
-		
-			fprintf(fp2,"\t");
-			fprintf(fp2,"%s\t",mtable[check].type); //DL
-			fprintf(fp2,"%d",mtable[check].ocode); //Opcode
-		}
-
-		else if(check>=50 && check<=54) //for registers(A,B,C,D)
-		{				//register number
-			fprintf(fp2,"\t");
-			fprintf(fp2,"%d",check-50);
-		}
-		else if(isdigit(label[count][0]) && count==0) //For literal table
-		{
-			fprintf(fp3,"%d \t",literal_no2); //To put in literal text file
-			fprintf(fp3,"%d\t\t",value);
-			fprintf(fp3,"%d\n\n",location_counter);
- 
-			fprintf(fp2,"%d",location_counter);//To put in IC file
+			case 2: //START
+				
+				m=search_POT(tok1);
+				if(m==1)
+				{	
+					lc=atoi(tok2);
+					fprintf(ip,"    (AD,01) (C,%d)\n",lc);
+				}
+				else if(m==5)//ORIGIN
+				{	
+					for(i=0;i<strlen(tok2);i++)
+					{
+						if(tok2[i]!='+')
+						{
+							tok4[i]=tok2[i];
+							
+						}
+						else 
+							break;
+						
+					}
+						tok4[i]='\0';
+						tok3[0]=tok2[i+1];
+						m3=atoi(tok3);
+						m1=search_st(tok4,counts);
+						m2=s[m1].s_add;
+						int x=lc;
+						lc=m2+m3;
+						fprintf(ip,"    (AD,0%d) (C,%d)\n",m,lc);
+						
+				}
+				
+				
+			break;
 			
-			fprintf(fp2,"\t\t\t");
-			fprintf(fp2,"\t00%d",value);
-			literal_no2++;
-		}	
-	
-		else if(isdigit(label[count][0])) //For literals and constants
-		{
-			if(L==1)
-			{
-				c='L';
-				fprintf(fp2,"\t%c\t",c); //literal L
-				fprintf(fp2,"%d",literal_no1); //literal no
-				literal_no1++;
-			}
-
-			else
-			{
-				c='C';fprintf(fp2,"\t\t%c\t",c); //Constant C
-				fprintf(fp2,"%d",value); //Value
-			}
-
-	  	}
-	   
-	    count++;
+			case 3:
+				// imperative statement
+				m=search_MOT(tok1);//MOVER
+				m1=search_REG(tok2);//AREG
+				m2=search_POT(tok2);//EQU
+				if(m2==4)//EQU
+				{
+					m3=search_st(tok1,counts);
+					if(m3==-1)
+					{
+						s[counts].s_no=counts+1;
+						strcpy(s[counts].s_name,tok1);	
+					}
+					m4=search_st(tok3,counts);
+					m5=s[m4].s_add;
+					s[counts].s_add=m5;
+					fprintf(ip,"    (S,%d) (AD,0%d) (S,%d)\n",counts+1,m2,m4+1);
+					counts++;
+					
+					
+				}
+				if(m>=1)
+				{
+					if(tok3[1]=='=')//litrals
+					{
+						l[countl].l_no=countl+1;
+						strcpy(l[countl].l_name,tok3);
+						fprintf(ip,"%d (IS,0%d) %d (L,%d) \n",lc,m,m1,l[countl].l_no);
+						lc++;
+						litral++;
+						countl++;
+					}
+					else //symbol
+					{
+						if(counts==0)
+						{
+							s[counts].s_no=counts+1;
+							strcpy(s[counts].s_name,tok3);
+						}
+						else
+						{
+							int sym=search_st(tok3,counts);
+							if(sym==-1)
+							{
+								s[counts].s_no=counts+1;
+								strcpy(s[counts].s_name,tok3);
+							}
+						}
+						
+						fprintf(ip,"%d (IS,0%d) %d (S,%d) \n",lc,m,m1,s[counts].s_no);
+						lc++;
+						counts++;
+					}
+				}
+				else 
+				{
+					if(strcmp(tok2,"DC")==0)
+					{
+						m3=search_st(tok1,counts);
+						s[m3].s_add=lc;
+						sadd=atoi(tok3);
+						fprintf(ip,"%d (S,%d) (DL,01) (C,%d) \n",lc,m3+1,sadd);
+						lc++;
+						
+					}
+					else if(strcmp(tok2,"DS")==0)
+					{
+						m3=search_st(tok1,counts);
+						s[m3].s_add=lc;
+						sadd=atoi(tok3);
+						fprintf(ip,"%d (S,%d) (DL,02) (C,%d) \n",lc,m3+1,sadd);
+						lc=lc+sadd-1;
+						lc++;
+						
+						
+					
+					}
+				}
+				
+				break;
+				case 4:
+					//label
+					s[counts].s_no=counts+1;
+					strcpy(s[counts].s_name,tok1);
+					s[counts].s_add=lc;
+					counts++;
+					m=search_MOT(tok2);
+					m1=search_REG(tok3);
+					if(m>=1)
+					{
+						if(tok4[1]=='=')//litrals
+						{
+							l[countl].l_no=countl+1;
+							strcpy(l[countl].l_name,tok4);
+							
+							fprintf(ip,"%d (L,0%d) (IS,0%d) %d (L,%d) \n",lc,l[countl-1].l_no,m,m1,l[countl].l_no);
+							lc++;
+							litral++;
+							countl++;
+						}
+						else	//symbol
+						{
+							
+						int sym=search_st(tok4,counts);
+						if(sym==-1)
+						{
+							s[counts].s_no=counts+1;
+							strcpy(s[counts].s_name,tok4);
+						}
+						
+						fprintf(ip,"%d (S,%d) (IS,0%d) %d (S,%d) \n",lc,s[counts-1].s_no,m,m1,s[counts].s_no);
+						counts++;
+						lc++;
+						}
+						
+					}
+				break;
+				
+				
+		}
 	}
-
-   location_counter++;
-   fputs("\n\n",fp2);
-
+	
+	fclose(fp);
+	return 0;	
+}
+void disp_symbol_table()
+{
+	//fprintf(tp,"\n*****Symbol Table*****\n");
+	//fprintf(tp,"-------------------------\n");
+	//fprintf(tp,"|S_no\t|S_name\t|S_addr\t|\n");
+	//fprintf(tp,"-------------------------\n");
+	for(int p=0;p<counts;p++)
+	{
+		  //fprintf(tp,"|%d\t|%s\t|%d\t|\n",s[p].s_no,s[p].s_name,s[p].s_add);
+		  fprintf(tp,"%d\t%s\t%d\t\n",s[p].s_no,s[p].s_name,s[p].s_add);
+	}
+	//fprintf(tp,"-------------------------\n");
 }
 
+void disp_litral_table()
+{
+	//fprintf(tp1,"\n*****Litral Table*****\n");
+	//fprintf(tp1,"-------------------------\n");
+	//fprintf(tp1,"|L_no\t|L_name\t|L_addr\t|\n");
+	//fprintf(tp,"-------------------------\n");
+	for(int p=0;p<countl;p++)
+	{
+		  //fprintf(tp,"|%d\t|%s\t|%d\t|\n",l[p].l_no,l[p].l_name,l[p].l_add);
+		  fprintf(tp1,"%d\t%s\t%d\t\n",l[p].l_no,l[p].l_name,l[p].l_add);
+	}
+	//fprintf(tp,"-------------------------\n");
+}
+
+void disp_pool_table()
+{
+	//fprintf(tp2,"\n\t*****POOLTAB*****\n");
+	//fprintf(tp2,"\t-----------------\n");
+	//fprintf(tp2,"\t|P_no\t|Index\t|\n");
+	//fprintf(tp2,"\t-----------------\n");
+	for(int p=0;p<countp;p++)
+	{
+		//fprintf(tp2,"\t|%d\t|%d\t|\n",(p+1),POOLTAB[p]);
+		fprintf(tp2,"\t%d\t%d\t\n",(p+1),POOLTAB[p]);
+	}
+	//fprintf(tp2,"\t-----------------\n");
+}
 
 int main()
 {
-	FILE *fp1, *fp2;
-	char temp[30];
-
-	fp1=fopen("input2.txt","r");
-	if(fp1==NULL)
-	{	printf("INPUT FILE CAN'T BE OPENED");
-		exit(1);
-	}
-
-// reading input.txt file
-
-while(!feof(fp1))
-{
-	fgets(temp,30,fp1);
-	printf("%s",temp);
-}
-//
-
-fp2=fopen("intermediate_code.txt","w");
-fputs("LC\tIC\tOP\tOPE1\tIC->OPE2\n\n",fp2);
-
-fp3=fopen("literal.txt","w");
-fputs("NO\tLiteral\t\tAddress\n\n",fp3);
-fcloseall();// closing all above file
-labels();
-
-printf("\nInput code-> input.txt\n");
-printf("intermediate code->intermediate_code.txt\n");
-printf("\nSymbol table->symbol.txt\n");
-printf("Literal table->literal.txt\n");
-
-//getch();
-
-return 0;
+	
+	printf("\nGenerate Intermediate code\n");
+	fp=fopen("prog1.asm","r");
+	ip=fopen("ic.txt","w");
+	pass1();
+	fclose(ip);
+	tp=fopen("symbol_table.txt","w");
+	tp1=fopen("litral_table.txt","w");
+	tp2=fopen("pool_table.txt","w");
+	disp_symbol_table();
+	disp_litral_table();
+	disp_pool_table();
+	fclose(tp);
+	fclose(tp1);
+	fclose(tp2);
+	
+	
+	
 }
